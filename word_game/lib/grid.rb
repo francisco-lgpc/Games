e886e55331ac973c9660eb07f80c642531cee5cb
Francisco Coutinho <francisco.lgpc@gmail.com>
2017-03-31 16:32:28 +0200

    added more vowels to grid

--- word_game/lib/grid.rb
+++ word_game/lib/grid.rb
@@ -4,13 +4,16 @@ class Grid
   def initialize(grid_size)
     # TODO: generate random grid of letters
     letters = []
+    vowels = ["a", "e", "i", "o", "u"]
     SCRABBLE_SCORES.each do |l, x|
       1.upto(120 / x) do
         letters << l
       end
     end
     @grid = []
-    1.upto(grid_size) { @grid << letters.sample }
+    1.upto(grid_size - 2) { @grid << letters.sample }
+    2.times { @grid << vowels.sample }
+    @grid.shuffle
   end
 
   def show
